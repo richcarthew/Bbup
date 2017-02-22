d0a8dcbe96f5b46a9d7c4ec22d1ba896278fa169
Rich Carthew <richcarthew@users.noreply.github.com>
2017-02-22 15:26:23 -0500

    Update popup copy.js
    notes on the obsolesce on the var searchURL in getImageUrl func

--- popup copy.js
+++ popup copy.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// *** IMPORTANT !!! *** - the var searchURL in getImageUrl func is obsolete, but this boiler plate is still instructional. 
+
 /**
  * Get the current URL.
  *
